88cdd0caef94d5c92dd7a9987d884d7e5f0df430
GodoPPL <godoppl@gmail.com>
2015-01-21 02:12:57 +0100

    FIrst commited version

---
+++ Line In VU Meter/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Line_In_VU_Meter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(346, 66);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Location = new System.Drawing.Point(12, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(346, 65);
+            this.panel1.TabIndex = 1;
+            this.panel1.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(7, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(340, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 85);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Show Device List";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Use selected Device:";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 7);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(162, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Show recording devices only";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(8, 115);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(350, 72);
+            this.panel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(370, 106);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(386, 230);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(386, 145);
+            this.Name = "Form1";
+            this.Text = "Volume level meter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Line In VU Meter/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.CoreAudioApi;
+
+namespace Line_In_VU_Meter
+{
+    public partial class Form1 : Form
+    {
+        static MMDeviceEnumerator deviceEnum = new MMDeviceEnumerator();
+        MMDevice device;
+        string[,] audioDevices = new string[255,2];
+        int i;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            enumerateDevices();
+            updateDevice();
+
+            try
+            {
+                checkBox1.Checked = Properties.Settings.Default.displayConfig;
+                checkBox2.Checked = Properties.Settings.Default.recordOnly;
+                comboBox1.SelectedIndex = Properties.Settings.Default.selectedDevice;
+            }
+            catch
+            {
+
+            }
+
+            if (Properties.Settings.Default.displayConfig == true)
+            {
+                this.Height = this.MaximumSize.Height;
+            }
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (device.AudioEndpointVolume.Mute.Equals(true))
+            {
+                panel1.Visible = true;
+            }
+            else
+            {
+                panel1.Visible = false;
+                try
+                {
+                    float volume = (float)device.AudioMeterInformation.MasterPeakValue * 200;
+                    progressBar1.Value = (int)volume;
+                }
+                catch 
+                {
+                    progressBar1.Value = 100;
+                }
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true) {
+                Properties.Settings.Default.displayConfig = true;
+                Properties.Settings.Default.Save();
+                this.Height = this.MaximumSize.Height;
+            }
+            else
+            {
+                Properties.Settings.Default.displayConfig = false;
+                Properties.Settings.Default.Save();
+                this.Height = this.MinimumSize.Height;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.selectedDevice = comboBox1.SelectedIndex;
+            Properties.Settings.Default.recordOnly = checkBox2.Checked;
+            Properties.Settings.Default.Save();
+            updateDevice();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.recordOnly = checkBox2.Checked;
+            enumerateDevices();
+        }
+
+        public void enumerateDevices()
+        {
+            comboBox1.Items.Clear();
+            i = 0;
+
+            if (Properties.Settings.Default.recordOnly == false)
+            {
+
+                foreach (MMDevice devices in deviceEnum.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active))
+                {
+                    audioDevices[i, 0] = devices.FriendlyName;
+                    audioDevices[i, 1] = devices.ID;
+                    comboBox1.Items.Add(audioDevices[i, 0].ToString());
+                    i++;
+                }
+            }
+            else
+            {
+                foreach (MMDevice devices in deviceEnum.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active))
+                {
+                    audioDevices[i, 0] = devices.FriendlyName;
+                    audioDevices[i, 1] = devices.ID;
+                    comboBox1.Items.Add(audioDevices[i, 0].ToString());
+                    i++;
+                }
+            }
+            
+        }
+        public void updateDevice()
+        {
+            try
+            {
+                device = deviceEnum.GetDevice(audioDevices[Properties.Settings.Default.selectedDevice, 1]);
+                this.Text = device.FriendlyName;
+            }
+            catch
+            {
+                Properties.Settings.Default.selectedDevice = 0;
+                if (audioDevices[0, 0] == null)
+                {
+                    MessageBox.Show("There are no devices to list!");
+                    this.Close();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+    }
+}
